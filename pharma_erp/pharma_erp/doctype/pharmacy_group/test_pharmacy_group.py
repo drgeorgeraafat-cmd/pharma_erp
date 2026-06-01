36f3f136f0b7ece3cb5e50053db551d42e22b6f7
Zee Pharaoh <dr.george.raafat@gmail.com>
2026-06-01 12:14:37 +0300

    Add Pharmacy helper Doctypes (Active Ingredient, Dosage Form, Group)

---
+++ pharma_erp/pharma_erp/doctype/pharmacy_group/test_pharmacy_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, ZeePharaoh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPharmacyGroup(FrappeTestCase):
+	pass
